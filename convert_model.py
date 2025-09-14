ee0e6b5ee69243b022f3fc6266fe5ce59f357766
ying zhan huang <booy99.com@gmail.com>
2025-09-14 22:50:00 +0800

    Update convert_model.py

--- convert_model.py
+++ convert_model.py
@@ -1,8 +1,8 @@
 from tensorflow.keras.models import load_model
 
-# 載入舊 .h5 模型
+# 載入舊模型
 old_model = load_model("models/lstm_model.h5")
 
 # 儲存為 SavedModel 格式
-old_model.save("models/lstm_model_v2")  # 目錄形式
+old_model.save("models/lstm_model_v2")  # 會生成 models/lstm_model_v2/ 資料夾
 print("Model converted to SavedModel format!")
